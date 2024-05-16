8542ce435e8ee83576124e82529b35ef89d9d3fc
Jochen Kupperschmidt <homework@nwsnet.de>
2024-05-16 02:59:12 +0200

    Allow to filter parties by country

--- src/lanpartydb_website/blueprints/party/views.py
+++ src/lanpartydb_website/blueprints/party/views.py
@@ -6,6 +6,8 @@ lanpartydb_website.blueprints.party.views
 :License: MIT
 """
 
+from collections import Counter
+
 from flask import abort, current_app, request
 from flask_paginate import Pagination
 from lanpartydb.models import Party
@@ -26,7 +28,7 @@ def index(page: int):
     per_page = request.args.get('per_page', type=int, default=50)
     offset = (page - 1) * per_page
 
-    parties = list(current_app.parties_by_slug.values())
+    parties = _get_parties()
     parties.sort(key=lambda party: party.start_on, reverse=True)
 
     pagination = Pagination(page=page, per_page=per_page, total=len(parties))
@@ -34,6 +36,54 @@ def index(page: int):
     parties_slice = parties[offset : offset + per_page]
 
     return {
+        'parties': parties_slice,
+        'pagination': pagination,
+        'countries': countries,
+    }
+
+
+@blueprint.get('/-/by-country/')
+@templated
+def index_countries():
+    counter = Counter()
+    for party in _get_parties():
+        if party.location:
+            counter[party.location.country_code] += 1
+
+    country_codes_with_party_count = list(counter.items())
+
+    countries_with_party_count = [
+        (_find_country(country_code), party_count)
+        for country_code, party_count in country_codes_with_party_count
+    ]
+
+    return {
+        'countries_with_party_count': countries_with_party_count,
+    }
+
+
+@blueprint.get('/-/by-country/<country_code>/', defaults={'page': 1})
+@blueprint.get('/-/by-country/<country_code>/pages/<int:page>/')
+@templated
+def index_by_country(country_code: str, page: int):
+    country = _find_country(country_code)
+
+    per_page = request.args.get('per_page', type=int, default=50)
+    offset = (page - 1) * per_page
+
+    parties = _get_parties()
+    parties = filter(
+        lambda party: _find_party_country_code(party) == country.alpha_2.lower(),
+        parties,
+    )
+    parties = list(sorted(parties, key=lambda party: party.start_on, reverse=True))
+
+    pagination = Pagination(page=page, per_page=per_page, total=len(parties))
+
+    parties_slice = parties[offset : offset + per_page]
+
+    return {
+        'country': country,
         'parties': parties_slice,
         'pagination': pagination,
         'countries': countries,
@@ -49,7 +99,7 @@ def view(slug: str):
 
     series = current_app.series_by_slug.get(party.series_slug)
 
-    country = _get_country(party)
+    country = _find_party_country(party)
 
     return {
         'party': party,
@@ -58,8 +108,24 @@ def view(slug: str):
     }
 
 
-def _get_country(party: Party) -> Country | None:
+def _get_parties() -> list[Party]:
+    return list(current_app.parties_by_slug.values())
+
+
+def _find_party_country_code(party: Party) -> str | None:
     if not party.location:
         return None
 
-    return countries.get(alpha_2=party.location.country_code)
+    return party.location.country_code
+
+
+def _find_party_country(party: Party) -> Country | None:
+    country_code = _find_party_country_code(party)
+    if not country_code:
+        return None
+
+    return _find_country(country_code)
+
+
+def _find_country(country_code: str) -> Country | None:
+    return countries.get(alpha_2=country_code)
